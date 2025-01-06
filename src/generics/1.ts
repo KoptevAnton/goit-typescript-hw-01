f61633be7a3a772824af51c48ae81ed8fa0a1b02
KoptevAnton <koptev110593@gmail.com>
2025-01-06 11:08:43 +0100

    Fix type of response.

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function fetchData<T extends object>(url: string): Promise<T> | never {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get<T>(url);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
